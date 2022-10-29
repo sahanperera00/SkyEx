ed78fc0598aab839c5498eb0878e1469dd33496a
Sahan Perera <sahanvimukthi00@gmail.com>
2022-10-29 15:02:19 +0530

    did some changes

--- app/src/main/java/com/example/skyex/LandingPage.java
+++ app/src/main/java/com/example/skyex/LandingPage.java
@@ -1,9 +1,6 @@
 package com.example.skyex;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,25 +9,25 @@ public class LandingPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing_page);
+        setContentView(R.layout.activity_landing_page1);
 
+//        Button btnLogin = findViewById(R.id.btn_login);
 
-        Button btnLogin = findViewById(R.id.btn_login);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LandingPage.this, LoginPage.class);
-                startActivity(intent);
-            }
-        });
-
-        Button btnSignup = findViewById(R.id.btn_signup);
-        btnSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LandingPage.this, SignupPage.class);
-                startActivity(intent);
-            }
-        });
+//        btnLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(LandingPage.this, LoginPage.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        Button btnSignup = findViewById(R.id.btn_signup);
+//        btnSignup.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(LandingPage.this, SignupPage.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
