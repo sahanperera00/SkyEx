9fdfaf85ca396ccbc95a97c24c73ba6e2148698f
IT21008382 <it21008382@my.sliit.lk>
2022-10-29 10:39:16 +0530

    add to cart button implemented

--- app/src/main/java/com/example/skyex/ProductActivity.java
+++ app/src/main/java/com/example/skyex/ProductActivity.java
@@ -1,22 +1,35 @@
 package com.example.skyex;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 public class ProductActivity extends AppCompatActivity {
-    private TextView productName, productPrice, productDescription;
+    private TextView productName, productPrice, productDescription, productSize;
     private ImageView productImage;
-    private ImageButton backProductButton;
+    private ImageButton backProductButton, cartButton;
+    private Button addtocartButton;
+    private TextInputEditText addQuantity;
     private String collectionName;
     private ProductModel productModel;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +38,18 @@ public class ProductActivity extends AppCompatActivity {
 
         productName = findViewById(R.id.idTVProductName);
         productPrice = findViewById(R.id.idTVProductPrice);
+        productSize = findViewById(R.id.idTVProductSize);
         productImage = findViewById(R.id.idIVProductImage);
         productDescription = findViewById(R.id.idTVProductDescription);
         backProductButton = findViewById(R.id.idIBBackProduct);
+        cartButton = findViewById(R.id.IDIBcart);
+        addtocartButton = findViewById(R.id.idAddToCartBtn);
+        addQuantity = findViewById(R.id.IDTIQuantity);
         collectionName = getIntent().getExtras().getString("collection");
         productModel = getIntent().getParcelableExtra("product");
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Carts");
 
         if (productModel!=null){
             productName.setText(productModel.getProductName());
@@ -38,6 +58,58 @@ public class ProductActivity extends AppCompatActivity {
             productDescription.setText(productModel.getProductDescription());
         }
 
+        DatabaseReference specificdatabasereference;
+        String ID = productName.getText().toString();
+        specificdatabasereference = firebaseDatabase.getReference("Carts").child(ID);
+
+
+
+        specificdatabasereference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                Object dbIDobject = snapshot.child("id").getValue();
+
+                if(dbIDobject == null){
+                    addtocartButton.setClickable(true);
+                }
+                else{
+                    String dbID = dbIDobject.toString();
+                    if(dbID.equals(ID)){
+                        addtocartButton.setClickable(false);
+                    }
+                    else
+                        addtocartButton.setClickable(true);
+                }
+
+            }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+
+        addtocartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = productName.getText().toString();
+                String quantity = addQuantity.getText().toString();
+                String price = productPrice.getText().toString();
+                String size = productSize.getText().toString();
+                String ID = name;
+
+                CartModel cartModel = new CartModel(name, size, quantity,price, ID);
+
+                databaseReference.child(ID).setValue(cartModel);
+                Toast.makeText(ProductActivity.this, "Product Added to the cart", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
         backProductButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,6 +117,14 @@ public class ProductActivity extends AppCompatActivity {
                 intent.putExtra("collection", collectionName);
                 startActivity(intent);
                 overridePendingTransition(0,0);
+            }
+        });
+
+        cartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProductActivity.this, ShoppingCartActivity.class);
+                startActivity(intent);
             }
         });
     }
